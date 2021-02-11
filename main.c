50addf4a742b8fa6a271c764de8c2aa2f5cd7632
Daniel Grace <dgrace@gracefulcode.com>
2021-02-10 23:21:27 -0600

    Add segmented string as a more generic version.

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 
 int main(int argc, char *argv[]) {
     struct segmented_string *ss = ss_create();
-    ss_print(ss);
 
-    ss_append_static_copy(ss, "hello ");
-    ss_print(ss);
+    ss_append_static_copy_static(ss, "hello world mate");
 
-    ss_append_placeholder_uint8(ss, "test");
-    ss_print(ss);
+    // ss_append_placeholder_uint8(ss, "test");
+    // ss_print(ss);
 
-    ss_append_static_copy(ss, "\n");
-    ss_print(ss);
+    ss_append_static_copy_static(ss, "\n");
 
-    for (uint8_t i = 0; i < 10; i++) {
-        struct segmented_string *ss_instance = ss_clone(ss);
-        ss_fill_uint8(ss_instance, "test", i);
-        ss_print(ss_instance);
-    }
+    struct segmented_string *ss2 = ss_explode_by_char(ss, ' ');
+    ss_print(ss2);
+
+    // for (uint8_t i = 0; i < 10; i++) {
+    //     struct segmented_string *ss_instance = ss_clone(ss);
+    //     ss_fill_uint8(ss_instance, "test", i);
+    //     ss_print(ss_instance);
+    // }
 
     return 0;
 }
